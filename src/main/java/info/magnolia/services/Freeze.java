35fa602422e4cb43731395cf7728d796a2b0fe23
Andrew Warinner <andrew.warinner@magnolia-cms.com>
2017-02-16 09:25:41 +0100

    fixed checkstyle complaints

--- src/main/java/info/magnolia/services/Freeze.java
+++ src/main/java/info/magnolia/services/Freeze.java
@@ -1,3 +1,17 @@
+/**
+ * This file Copyright (c) 2017 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Magnolia Network Agreement
+ * which accompanies this distribution, and is available at
+ * http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
 package info.magnolia.services;
 
 import info.magnolia.module.ModuleLifecycle;
@@ -6,6 +20,9 @@ import info.magnolia.module.ModuleLifecycleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * .
+ */
 public class Freeze implements ModuleLifecycle {
 
     private static final Logger logger = LoggerFactory.getLogger(Freeze.class);
